9e95be45c596b1e763ef9ba03781190f050c83b8
allendolph <allendolph@iteontech.com>
2011-12-27 17:03:19 -0800

    Added user and profile

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111223181633) do
+ActiveRecord::Schema.define(:version => 20111228003926) do
 
   create_table "articles", :force => true do |t|
     t.datetime "created_at"
@@ -20,6 +21,24 @@ ActiveRecord::Schema.define(:version => 20111223181633) do
     t.datetime "published_at"
     t.string   "excerpt"
     t.string   "location"
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "color"
+    t.string   "twitter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
